ec8a88495a6a638328a637b7aa3918be97b1b428
habac99 <havanbac99@gmail.com>
2019-05-06 16:15:03 +0700

    học sinh đăng ký

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: habac
+ * Date: 06-May-19
+ * Time: 2:27 PM
+ */
+
+
+$connect = mysqli_connect(
+    'localhost', 'root', '', 'cnpmdatabase'
+    )
+
+or
+die("Không thể kết nối database");
